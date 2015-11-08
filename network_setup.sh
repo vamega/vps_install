58b8305db0d7994417f939871ba599b1667e0229
Varun Madiath <vamega@gmail.com>
2015-11-09 01:14:18 +0530

    Improved networking.
    * Added networking bootstrap commands (necessary due to lack of DHCP and
      valid routes in installer environment).
    * Added resolv.conf for installer environment
    * Switched IPv6 addresss
    * Moved more configuration to variables.sh

---
+++ network_setup.sh
@@ -0,0 +1,14 @@
+# These commands might need to be manually typed in during a VNC session
+# since they are necessary to set up the network.
+
+source variables.sh 
+
+ip addr add ${IPADDR4}/32 broadcast ${IPADDR4} dev ${INSTALLER_DEVICE_NAME}
+ip route add ${GATEWAY4}/32 dev ${INSTALLER_DEVICE_NAME}
+ip route add default via ${GATEWAY4} dev ${INSTALLER_DEVICE_NAME}
+
+echo "nameserver 8.8.8.8 > /etc/resolv.conf"
+
+passwd
+echo "PasswordAuthentication no"
+systemctl start sshd
